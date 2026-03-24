761b9497a8ce63603162e77750ab23e447b58f1f
Yazeed Alfuqaha <yalfuqaha8@gmail.com>
2026-03-24 11:29:02 +0300

    &&

--- src/main.js
+++ src/main.js
@@ -1,12 +1,5 @@
-import { createApp } from 'vue';
-import App from './App.vue';
-import router from './router';
-import { createPinia } from 'pinia';
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
 
-const app = createApp(App);
-
-const pinia = createPinia();
-app.use(pinia);
-app.use(router);
-
-app.mount('#app');
+createApp(App).use(router).mount('#app')
